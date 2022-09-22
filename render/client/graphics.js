a9a85077723b83187e65e2bba34fef68677e80c2
Nate Schultz <ngs32@drexel.edu>
2022-09-21 22:25:09 -0400

    tweaks

--- render/client/graphics.js
+++ render/client/graphics.js
@@ -1,110 +1,103 @@
+
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-import { OrbitControls } from './jsm/controls/OrbitControls.js';
+const SCREEN_WIDTH = window.innerWidth;
+const SCREEN_HEIGHT = window.innerHeight;
 
-let cameraPersp, cameraOrtho, currentCamera;
-let scene, renderer, orbit;
-let sphere;
-let user_lat, user_long, user_rotation;
-let user_x, user_y, user_z;
+let container, stats;
+let camera, scene, renderer;
 
-init();
-render();
+await init();
+animate();
 
-function asCartesian(r, theta, phi) {
-    const x = r * Math.sin(theta) * Math.cos(phi)
-    const y = r * Math.sin(theta) * Math.sin(phi)
-    const z = r * Math.cos(theta)
-    return [x, y, z]
-}
+async function init() {
 
-function init() {
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
 
-    // get user location
-    const urlParams = new URLSearchParams(window.location.search);
-    user_lat = urlParams.get('lat');
-    user_long = urlParams.get('long');
-    user_rotation = urlParams.get('rotation');
-    const cartesian = asCartesian(200, user_lat, user_long);
-    user_x = cartesian[0]
-    user_y = cartesian[1]
-    user_z = cartesian[2]
+    // CAMERA
 
-    renderer = new THREE.WebGLRenderer({ alpha: true });
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    camera = new THREE.PerspectiveCamera( 40, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 10000 );
+    camera.position.set( 500, 1000, - 500 );
 
-    const aspect = window.innerWidth / window.innerHeight;
-
-    // add camera
-    cameraPersp = new THREE.PerspectiveCamera( 50, aspect, 0.01, 30000 );
-    cameraOrtho = new THREE.OrthographicCamera( - 600 * aspect, 600 * aspect, 600, - 600, 0.01, 30000 );
-    currentCamera = cameraPersp;
-
-    currentCamera.position.set( 500, 250, 500 );
-    currentCamera.lookAt( 0, 200, 0 );
+    // SCENE
 
     scene = new THREE.Scene();
 
-    // add textured sphere
-    const texture_world = new THREE.TextureLoader().load( 'textures/grid512.jpg', render );
-    texture_world.anisotropy = renderer.capabilities.getMaxAnisotropy();
+    // LIGHTS
 
-    sphere = new THREE.SphereGeometry( 200, 200, 200 );
-    const globe_material = new THREE.MeshLambertMaterial( { map: texture_world, transparent: true } );
-    const globe_mesh = new THREE.Mesh( sphere, globe_material )
-    scene.add( globe_mesh );
+    const light = new THREE.DirectionalLight( 0xaabbff, 0.3 );
+    light.position.x = 300;
+    light.position.y = 250;
+    light.position.z = - 500;
+    scene.add( light );
 
-    // add user location indicator
-    const texture_cursor = new THREE.TextureLoader().load( 'textures/cursor2.png', render );
-    texture_cursor.anisotropy = renderer.capabilities.getMaxAnisotropy();
+    // SKYDOME
 
-    const cursor = new THREE.SphereGeometry( 5, 5, 5 );
-    const cursor_material = new THREE.MeshLambertMaterial( { map: texture_cursor, transparent: true } );
-    const cursor_mesh = new THREE.Mesh( cursor, cursor_material )
-    scene.add( cursor_mesh );
-    cursor_mesh.position.set(user_x, user_y, user_z);
+    const vertexShader = document.getElementById( 'vertexShader' ).textContent;
+    const fragmentShader = document.getElementById( 'fragmentShader' ).textContent;
+    const uniforms = {
+        topColor: { value: new THREE.Color( 0x0077ff ) },
+        bottomColor: { value: new THREE.Color( 0xffffff ) },
+        offset: { value: 400 },
+        exponent: { value: 0.6 }
+    };
+    uniforms.topColor.value.copy( light.color );
 
-    // todo: rotate cursor to where the user is facing
+    const skyGeo = new THREE.SphereGeometry( 4000, 32, 15 );
+    const skyMat = new THREE.ShaderMaterial( {
+        uniforms: uniforms,
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader,
+        side: THREE.BackSide
+    } );
 
-    // add lighting
+    const sky = new THREE.Mesh( skyGeo, skyMat );
+    scene.add( sky );
 
-    // todo; add sun that rotates corresponding to in game time
-    // const light = new THREE.DirectionalLight( 0xf0f0f0, 2 );
-    // light.position.set( 1, 1, 1 );
-    // scene.add( light );
+    // RENDERER
 
-    const ambientLight = new THREE.AmbientLight( 0xffffff ); // soft white light
-    scene.add( ambientLight );
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
+    container.appendChild( renderer.domElement );
+    renderer.outputEncoding = THREE.sRGBEncoding;
 
-    // add mouse interactions
-    orbit = new OrbitControls( currentCamera, renderer.domElement );
-    orbit.update();
-    orbit.addEventListener( 'change', render );
+    // CONTROLS
+
+    const controls = new OrbitControls( camera, renderer.domElement );
+    controls.maxPolarAngle = 0.9 * Math.PI / 2;
+    controls.enableZoom = true;
+
+    // MODEL
+
+    const loader = new THREE.ObjectLoader();
+    const object = await loader.loadAsync( './models/json/lightmap/lightmap.json' );
+    scene.add( object );
+
+    //
 
     window.addEventListener( 'resize', onWindowResize );
+
 }
 
 function onWindowResize() {
 
-    const aspect = window.innerWidth / window.innerHeight;
-
-    cameraPersp.aspect = aspect;
-    cameraPersp.updateProjectionMatrix();
-
-    cameraOrtho.left = cameraOrtho.bottom * aspect;
-    cameraOrtho.right = cameraOrtho.top * aspect;
-    cameraOrtho.updateProjectionMatrix();
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
     renderer.setSize( window.innerWidth, window.innerHeight );
 
-    render();
-
 }
 
-function render() {
+//
 
-    renderer.render( scene, currentCamera );
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    renderer.render( scene, camera );
+    stats.update();
 
 }
--- render/render.js
+++ render/render.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3002
+const port = 3000
 
 app.use(express.static(__dirname + '/client'));
-app.get("/globe", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client/sphere.html");
 });
 
